33733573d3f01385606154dacd7dd5844ef2d10d
YunjaeGong <llegokid@naver.com>
2021-02-15 17:03:22 +0900

    .

--- src/main/java/com/yunjae/blog/test/HttpControllerTest.java
+++ src/main/java/com/yunjae/blog/test/HttpControllerTest.java
@@ -1,4 +1,4 @@
-package com.yunjae.blog;
+package com.yunjae.blog.test;
 
 import org.springframework.web.bind.annotation.*;
 
@@ -9,15 +9,16 @@ public class HttpControllerTest{
 
     // http://localhost:8080/http/get (select)
     @GetMapping("/http/get")
-    public String getTest() {
-        return "get 요청";
+    public String getTest(Member m) { // query string 데이터를 오브젝트에 스프링이 넣어줌
+        return "get 요청:" + m.getId() + ", " + m.getUsername()  + ", " + m.getPassword() + ", " + m.getEmail();
     }
 
     // 브라우저에서는 get 요청만 할 수 있다. : 405에러
     // http://localhost:8080/http/post (insert)
     @PostMapping("/http/post")
-    public String postTest() {
-        return "post 요청";
+    public String postTest(@RequestBody String text) {
+        // return "post 요청:" + m.getId() + ", " + m.getUsername()  + ", " + m.getPassword() + ", " + m.getEmail();
+        return "post 요청: " + text;
     }
 
     // http://localhost:8080/http/put (update)
